02ff626e23615550681bfd8a79a4d3ff5866bc38
mdaxc6 <mikeabsher1128@gmail.com>
2021-05-10 19:33:30 -0500

    updated js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,40 @@
 // from data.js
 var tableData = data;
 
-var ufoTableBody = d3.select("#ufo-table>tbody");
-
-data.forEach((ufoSighting) => {
-    var row = ufoTableBody.append("tr");
-    Object.values(ufoSighting).forEach(value => {
-        var cell = row.append("td");
-        cell.text(value);
+function printTable(data){
+    var ufoTableBody = d3.select("#ufo-table>tbody");
+    data.forEach((ufoSighting) => {
+        var row = ufoTableBody.append("tr");
+        Object.values(ufoSighting).forEach(value => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
+};
+
+// adds data to table
+printTable(tableData);
+
+// filtering by date
+var dateBtn = d3.select("#filter-btn");
+//var form = d3.select("form");
+
+dateBtn.on("click", () => {
+    d3.event.preventDefault();
+    var input = d3.select("#datetime");
+    var value = input.property("value");
+    var filtered = data.filter(sighting => sighting.datetime == value);
+    printTable(filtered);
 });
+
+// form.on("submit", () => {
+//     d3.event.preventDefault();
+//     var input = d3.select("#datetime");
+//     var value = input.property("value");
+
+//     var filtered = data.filter(sighting => sighting.date == input);
+
+//     printTable(filtered);
+// });
+
 
